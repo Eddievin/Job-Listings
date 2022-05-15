372bae5bbd658f0f7e1143ec5e096b27822d3a01
Eddievin <thokerunga200@gmail.com>
2022-05-15 12:23:24 +0400

    updated code

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -38,7 +38,11 @@
 
         @include('partials.footer')
 
-        
+        <script src="{{ asset('js/vendor/jquery-2.2.4.min.js') }}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/vendor/bootstrap.min.js') }}"></script>
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+        <script src="{{ asset('js/easing.min.js') }}"></script>	
         <script src="{{ asset('js/hoverIntent.js') }}"></script>
         <script src="{{ asset('js/superfish.min.js') }}"></script>
         <script src="{{ asset('js/jquery.ajaxchimp.min.js') }}"></script>
